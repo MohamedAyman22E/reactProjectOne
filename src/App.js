18c2cfc6e499633b788b44ff1f08f4f0ba337931
moAymanEz <moAyman@gmail.com>
2023-11-20 04:34:56 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Contact from "./Component/Contact/Contact";
+import Features from "./Component/Features/Features";
+import Header from "./Component/Head/Header";
+import Hero from "./Component/Hero/Hero";
+import Portfolio from "./Component/Portfolio/Portfolio";
+import Resume from "./Component/Resume/Resume";
+import Testimonial from "./Component/Testimonial/Testimonial";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header />
+      <Hero />
+      <Features />
+      <Portfolio />
+      <Resume />
+      <Testimonial />
+      <Contact />
+    </>
   );
 }
-
 export default App;
